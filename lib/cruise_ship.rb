3e485cf1e31f8816d881421dc60728ed26bb5ec1
beatific-queue-4949 <zivkovicmilan1987@gmail.com>
2020-11-23 04:34:36 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -10,7 +10,7 @@ def select_winner(passengers)
   # add the code snippet here!
   winner = ""
   passengers.each do |suite, name|
-    if (suite == ":suite_a" && name.start_with?("A"))
+    if (suite == :suite_a && name.start_with?("A"))
       winner = name
     end
   end
